e2a52dd71f46d4c92a3608d1320e16c5b0c4ccaa
zyw0605688 <zyw0605688@163.com>
2023-06-30 18:56:35 +0800

    feat:'http修复'

--- web/src/api/deployment.ts
+++ web/src/api/deployment.ts
@@ -1,5 +1,12 @@
 import { http } from "@/utils/http";
 
+export const AddDeployment = (params) => {
+  return http.post("/deployment",params)
+}
+
+export const DeleteDeployment = (params) => {
+  return http.delete("/deployment",params)
+}
 export const DeploymentList = (params) => {
   return http.get("/deployment",params)
 }
